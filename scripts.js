2ac4780617997f28811bba9dca0f0c17c67e641f
Alex Dunne <a.dunne@braiswick.co.uk>
2018-01-23 15:29:57 +0000

    Remove ScrollMagic. Add Scrollify and AnimateIt

--- scripts.js
+++ scripts.js
@@ -1,35 +1,22 @@
-$(document).ready(function (){                
-    
-    if ($(window).width() > 1024){
-        console.log($(window).width());
-        
-        var controller = new ScrollMagic.Controller();
-        
-        var wipeAnimation = new TimelineMax()
-            .to("logo", 20, {x: "-50%", y: "20%", scale: 0.5})
-            .fromTo("section.slide.slide-two", 20, {opacity: "0"}, {opacity: "1", ease: Linear.easeNone})
-            .to("section.slide.slide-two", 40, {opacity: "1"})
-            .fromTo("section.slide.slide-four", 20, {opacity: "0"}, {opacity: "1", ease: Linear.easeNone})
-            .fromTo("section.slide.slide-five", 20, {opacity: "0"}, {opacity: "1", ease: Linear.easeNone})
-            .to("section.slide.slide-five", 40, {opacity: "1"})
-            .fromTo("section.slide.slide-six", 20, {opacity: "0"}, {opacity: "1", ease: Linear.easeNone})
-            .fromTo("section.slide.slide-seven", 20, {opacity: "0"}, {opacity: "1", ease: Linear.easeNone})
-            .to("section.slide.slide-seven", 40, {opacity: "1"});
+$.scrollify({
+    section : ".slide",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: false,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
 
-        new ScrollMagic.Scene({
-            triggerElement: "#pinContainer",
-            triggerHook: "onLeave",
-            duration: "300%"
-        })
-            .setPin("#pinContainer")
-            .setTween(wipeAnimation)
-            .addIndicators()
-            .addTo(controller);
-        
-            controller.scrollTo(function (newpos){
-            TweenMax.to(window, 0.5, {scrollTo: {y: newpos}, ease: Cubic.easeInOut});
-        });
-    }
+$(document).ready(function (){      
 
     $('.loader-container').fadeOut('slow');
 
