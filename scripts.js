555df011870f8c9b6be7c8e8acefbe6af8963ff4
Alex Dunne <a.dunne@braiswick.co.uk>
2018-01-23 16:15:55 +0000

    Edit images and tweak layout. Remove excess

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ $.scrollify({
     offset : 0,
     scrollbars: true,
     standardScrollElements: "",
-    setHeights: true,
+    setHeights: false,
     overflowScroll: true,
     updateHash: false,
     touchScroll:true,
